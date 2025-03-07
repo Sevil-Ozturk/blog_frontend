f69435867b8f80bde66f92f7caf7d4193cd22bad
Sevil Öztürk <ozsevil730@gmail.com>
2025-03-07 18:52:34 +0300

    increase the capacity and fix

---
+++ components/modal/newpost.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+
+const postsStore = usePostsStore();
+
+onMounted(async () => {
+  await postsStore.fetchPosts()
+})
+
+const isOpen = ref(false);
+
+const title = ref('');
+const content = ref('');
+const image = ref<File | null>(null);
+const tags = ref<string[]>([]);
+
+const tagOptions = ['Vue.js', 'JavaScript', 'Frontend', 'Backend', 'UI/UX','Node.JS'];
+
+const convertImageToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+const handleAddPost = async () => {
+  const newPost = {
+    title: title.value,
+    content: content.value,
+    image: image.value ? await convertImageToBase64(image.value) : "",
+    tags: tags.value,
+  };
+  // console.log("Gönderilen Post:", newPost);
+  //postStore daki addPost fonksiyonuna gönderiyorum :D             
+  await postsStore.addPost(newPost);
+
+  title.value = '';
+  content.value = '';
+  image.value = null;
+  tags.value = [];
+  isOpen.value = false;
+};
+
+const handleImageChange = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files[0]) {
+    image.value = input.files[0];
+  }
+};
+</script>
+
+<template>
+   <div class="fixed right-0 m-7 absolute">
+      <UButton label="Yeni Post Ekle" @click="isOpen = true" :style="{backgroundColor:'#dd91f4', color: '#313840' }"/>
+    </div>
+    <UModal v-model="isOpen" prevent-close >
+      <UCard>
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-base font-semibold">Yeni Post Ekle</h3>
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" @click="isOpen = false" />
+          </div>
+        </template>
+
+        <form @submit.prevent="handleAddPost" class="space-y-4 p-4">
+          <div >
+            <label for="image" class="block text-sm font-medium text-[#FDDD88] my-2">Resim:</label>
+            <input id="image" type="file" accept="image/*" @change="handleImageChange"  class="mt-1 p-1 block w-full text-sm text-gray-500 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 bg-[#F2E9EB]"/>
+
+            <label for="title" class="block text-sm font-medium text-[#FDDD88] my-2">Başlık:</label>
+            <input id="title" type="text" v-model="title" placeholder="Lütfen başlık girin." required class="bg-[#F2E9EB] w-full border rounded p-1 text-black"/>
+
+            <label for="content" class="block text-sm font-medium text-[#FDDD88] my-2">İçerik:</label>
+            <textarea id="content" v-model="content" placeholder="Lütfen içeriği girin." required class="bg-[#F2E9EB] w-full border rounded p-1 h-[250px] text-black whitespace-pre"></textarea>
+
+            <label for="tags" class="block text-sm font-medium text-[#FDDD88] my-2">Etiketler:</label>
+            <USelectMenu id="tags" v-model="tags" :options="tagOptions" multiple  />
+
+            <div class="container justify-end items-end my-3">
+            <UButton type="submit" label="Ekle" :style="{ backgroundColor: '#DD8EA3', color: 'white' }" />
+              </div>
+          </div>
+        </form>
+      </UCard>
+    </UModal>
+    
+</template>
--- stores/posts.ts
+++ stores/posts.ts
@@ -23,41 +23,40 @@ export const usePostsStore = defineStore('posts', () => {
   const addPost = async (newPost : Post) => {
     const toast =useToast();
 try{
+    const response = await fetch('http://localhost:5000/', {
+      method:'POST',
+      headers:{
+        "Content-Type":"Application/json",
+      },
+      body:JSON.stringify(newPost)
+    });
 
-const response = await fetch('http://localhost:5000/', {
-  method:'POST',
-  headers:{
-    "Content-Type":"Application/json"
-  },
-  body:JSON.stringify(newPost)
-});
+    const data = await response.json(); 
 
-const data = await response.json(); 
+    if(data.value){
+      posts.value.push(data.value)
+      postCount.value++
+      toast.add({
+        title: 'Başarılı!',
+        description: 'Yeni post başarıyla eklendi.',
+        color: 'green',
+        icon: 'i-heroicons-check-circle',
+        timeout: 3000 
+      })
+      await fetchPosts();
+    }
 
-if(data.value){
-  posts.value.push(data.value)
-  postCount.value++
-  toast.add({
-    title: 'Başarılı!',
-    description: 'Yeni post başarıyla eklendi.',
-    color: 'green',
-    icon: 'i-heroicons-check-circle',
-    timeout: 3000 
-  })
-  await fetchPosts();
-}
+  }catch(error){
 
-}catch(error){
-
-  console.error('Post oluşturulurken hata meydana geldi!!!',error)
-  toast.add({
-    title: 'Hata!',
-    description: 'Post eklenirken bir hata oluştu.',
-    color: 'red',
-    icon: 'i-heroicons-x-mark-20-solid',
-    timeout: 3000,
-  });
-}
+    console.error('Post oluşturulurken hata meydana geldi!!!',error);
+    toast.add({
+      title: 'Hata!',
+      description: 'Post eklenirken bir hata oluştu.',
+      color: 'red',
+      icon: 'i-heroicons-x-mark-20-solid',
+      timeout: 3000,
+    });
+  }
 
   }
 
@@ -66,33 +65,5 @@ if(data.value){
     fetchPosts,
     addPost,
   }
+
 })
-
-
-// import type { Post, PostResponse } from '~/types'
-
-// export const usePostsStore = defineStore('posts', () => {
-//   const posts = ref<Post[]>([])
-
-//   const fetchPosts = async () => {
-//     try {
-//       const { data, error } = await useFetch<PostResponse>('http://localhost:5000')
-
-//       if (error.value) {
-//         throw new Error(error.value.message)
-//       }
-
-//       if (data.value) {
-//         posts.value = data.value.posts // API'den gelen 'posts' dizisine erişim
-//         console.log('Veriler başarıyla çekildi:', posts.value)
-//       }
-//     } catch (error) {
-//       console.error("Postları çekerken hata oluştu:", error)
-//     }
-//   }
-
-//   return {
-//     posts,
-//     fetchPosts,
-//   }
-// })
